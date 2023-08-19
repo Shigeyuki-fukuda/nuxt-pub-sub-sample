3d3b2c2c33175af261ea059a03a854bfeb236607
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2023-08-19 22:47:47 +0900

    feat: Tailwind CSSディレクティブをグローバルに読み込むようにした

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   // https://tailwindcss.com/docs/guides/nuxtjs#3
+  css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
