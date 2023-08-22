240148964fc16922ba19a889cf337f8649487623
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2023-08-22 09:07:46 +0900

    feat: トップページの土台をマークアップ

---
+++ components/Card2.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'Card2',
+}
+</script>
+
+<template>
+  <div class="flex flex-col justify-start items-center">
+    <div class="bg-white shadow-md rounded-md p-6 w-[500px]">
+      <h2 class="text-xl font-semibold text-gray-800 mb-2 sm:text-2xl">Card Title1</h2>
+      <p class="text-gray-600 mb-4 sm:text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt justo quis eros tincidunt, at pellentesque erat tristique.</p>
+      <button class="py-4 px-4 mt-8 text-2xl bg-emerald-500 hover:bg-emerald-600 focus:ring-emerald-600 focus:ring-offset-emerald-600 text-white transition ease-in duration-200 text-center font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full">
+        Submit
+      </button>
+    </div>
+  </div>
+</template>
