37ee9cf88fe88908d4aa2b5ef670f8cfafa52a18
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2023-08-29 22:00:44 +0900

    feat: モーダルを開くイベント発行とサブスクのテンプレート側の処理を実装

--- components/Card2.vue
+++ components/Card2.vue
@@ -7,9 +7,11 @@ export default {
 <template>
   <div class="flex flex-col justify-start items-center">
     <div class="bg-white shadow-md rounded-md p-6 w-[500px]">
-      <h2 class="text-xl font-semibold text-gray-800 mb-2 sm:text-2xl">Card Title1</h2>
+      <h2 class="text-xl font-semibold text-gray-800 mb-2 sm:text-2xl">Card Title2</h2>
       <p class="text-gray-600 mb-4 sm:text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt justo quis eros tincidunt, at pellentesque erat tristique.</p>
-      <button class="py-4 px-4 mt-8 text-2xl bg-emerald-500 hover:bg-emerald-600 focus:ring-emerald-600 focus:ring-offset-emerald-600 text-white transition ease-in duration-200 text-center font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full">
+      <button
+        class="rounded-full w-[120px] bg-indigo-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
         Submit
       </button>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,19 @@ export default {
 <script setup lang="ts">
 import Card1 from "../components/Card1.vue";
 import Card2 from "../components/Card2.vue";
+import { useModalDisplayEvent } from '~/composables/events/modal-display';
+
+const modalVisible = ref(false);
+const { on } = useModalDisplayEvent();
+on((event) => {
+  if (event.action === "OPEN_MODAL") {
+    modalVisible.value = true;
+  }
+
+  if (event.action === "CLOSE_MODAL") {
+    modalVisible.value = false;
+  }
+});
 </script>
 
 <template>
